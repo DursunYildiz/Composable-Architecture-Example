8baf89047a38613fd29afd2bf8a3b65b82715618
Dursun Yıldız <duyildiz@innova.com.tr>
2022-10-10 15:46:51 +0300

    Initial Commit

---
+++ ComposableExample/ComposableExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableExampleApp.swift
+//  ComposableExample
+//
+//  Created by Dursun YILDIZ on 10.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ComposableExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
